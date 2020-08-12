7e8611c1f69dcbea3737619968bf37a59b21766a
kortewegdevries <kortewegdevries@protonmail.ch>
2020-08-12 12:49:37 +0530

    Cleanup my working branch

--- arch/ppc64/config.sh
+++
@@ -1,7 +0,0 @@
-# $Id$
-#
-# This file is sourced AFTER defaults/config.sh; generic options should be set there.
-# Arch-specific options that normally shouldn't be changed.
-#
-KERNEL_MAKE_DIRECTIVE="vmlinux"
-KERNEL_BINARY="vmlinux"
